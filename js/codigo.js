3a7742d2f408fe9bd7206d973b8184391a6b1033
Brayan Basallo <basallo17@gmail.com>
2019-12-30 00:25:27 -0500

    Funciona perfectamente, no hay conflicto en los ciclos

--- js/codigo.js
+++ js/codigo.js
@@ -1,8 +1,13 @@
 console.log("Hola mundo!");
 
+var accion = true;
 var arregloNav = [];
 var tablero = document.getElementById("tablero");
 
+/**
+ * Se crea el arreglo para dar forma al arbol
+ * Se limpia el tablero donde se pinta el arbol
+ */
 function crearArreglo() {
     arregloNav.push(["*"]);
     arregloNav.push(["*", "*"]);
@@ -15,24 +20,23 @@ function crearArreglo() {
     arregloNav.push(["|","|"])
     arregloNav.push(["|","|"])
     tablero.innerHTML = "";
-
     imprimirArreglo();
 }
 
+/**
+ * Esta funcion se encarga de pintar en pantalla para fibujar
+ * el arbolito
+ */
 function imprimirArreglo() {
     let i, o, color, color1;
     tablero.innerHTML += "<span style='color:#F1C40F'>*</span><br>"
     for (i = 0; i < arregloNav.length; i++) {
         for (o = 0; o < arregloNav.length; o++) {
-            /* if (arregloNav[i][o] == "*") {
-                color = luces(arregloNav[i].length);
-            } */
             switch (arregloNav[i][o]) {
                 case "*":
                     color = luces(arregloNav[i].length);
                     color1 = luces(arregloNav[i].length);
                     tablero.innerHTML += "<span style='color:" + color + "'>*</span><span style='color:" + color1 + "'>*</span>"
-                    /* tablero.innerHTML += "<span style='color:" + color + "'>*</span>" */
                     break;
                 case "|":
                     tablero.innerHTML += "<span style='background-color:#AF601A; color:#AF601A'>|</span>"
@@ -43,15 +47,16 @@ function imprimirArreglo() {
         }
         tablero.innerHTML += "<br>";
     }
-    /* console.log("aqui"); */
     arregloNav = [];
 }
 
+/**
+ * se encarga de devolever un color aleatoreo 
+ * @param {ser recibe el número de objetos en el arreglo} ArregloLength 
+ */
+
 function luces(ArregloLength) {
-    /* console.log(ArregloLength); */
-
     let number = numeroAleratorio(ArregloLength);
-
     let salida;
     switch (number) {
         case 1:
@@ -67,17 +72,36 @@ function luces(ArregloLength) {
             salida = "#34495E";
             break;
     }
-    /* console.log(number)
-    console.log(salida); */
     return salida;
 }
+
+/**
+ * genera un numero automatico
+ * @param {numero de a cantidad del arreglo} condicion 
+ */
 
 function numeroAleratorio(condicion) {
     let salida = Math.floor(Math.random() * (0 + condicion) - 0);
     return salida;
 }
-crearArreglo();
-/* setInterval(() => {
-    tablero.innerHTML = "";
-}, 1000); */
-setInterval(crearArreglo, 1000)
+
+/**
+ * Se encarga de empezar el ciclo de dibujó
+ */
+function imprimir(){
+    if(accion){
+        crearArreglo()
+    }
+}
+
+/**
+ * Se encarga deocultar el arbol para mostrar mensaje
+ */
+function cambio(){    
+    accion != false;
+    tablero.classList.toggle("ocultar");
+}
+
+setInterval(imprimir, 1000);
+setInterval(cambio,5000)
+
